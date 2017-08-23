d77583eb4e726a3f0cc79f0867f536ef78f3446c
Akash Bhardwaj <akash.bhardwaj@myntra.com>
2017-08-23 23:56:59 +0530

    map integrated, location marker setup - location detection pending

--- App.js
+++ App.js
@@ -1,22 +1,2 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MapView } from 'expo';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Test 1</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Espy from './app/index'
+export default Espy
---
+++ app/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { Constants, MapView } from 'expo';
+import { MAP_STYLE_NIGHT }  from './config'
+
+export default class Espy extends React.Component {
+  state = {
+    mapRegion: {
+      latitude: 19.1197,
+      longitude: 72.9051,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    }
+  }
+
+  _handleMapRegionChange = (mapRegion) => {
+    this.setState({ mapRegion })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={MapView.PROVIDER_GOOGLE}
+          customMapStyle={MAP_STYLE_NIGHT}
+          style={styles.map}
+          region={this.state.mapRegion}
+          onRegionChange={this._handleMapRegionChange}>
+            <MapView.Marker coordinate={{ latitude: 19.1197, longitude: 72.9051 }}>
+              <View style={styles.radius}>
+                <View style={styles.marker} />
+              </View>
+            </MapView.Marker>
+        </MapView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50/2,
+    backgroundColor: 'rgba(128,128,128, 0.2)',
+    borderWidth: 1,
+    borderColor: 'rgb(128,128,128)',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  marker : {
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 20/2,
+    overflow: 'hidden',
+    backgroundColor: '#007FAA',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+});
