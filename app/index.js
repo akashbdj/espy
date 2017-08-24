3cfc474449a1a18ec10eb5982e25db8cee8125d0
akash-bhardwaj <akashbdj@gmail.com>
2017-08-24 23:58:20 +0530

    google places autocomplete added

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { Permissions, Location, Constants, MapView } from 'expo';
 import { MAP_STYLE_SILVER } from './configs/map-config'
+import GooglePlacesInput from './places'
 
 const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 };
 
@@ -40,12 +41,10 @@ export default class Espy extends React.Component {
 
     return (
       <View style={styles.container}>
+        <GooglePlacesInput />
         <MapView
           showsMyLocationButton={true}
-          showsPointsOfInterest={true}
-          showsScale={true}
           toolbarEnabled={true}
-          legalLabelInsets={{ top: 0, bottom: 0, right: 0, left: 0 }}
           showsUserLocation={true}
           provider={MapView.PROVIDER_GOOGLE}
           customMapStyle={MAP_STYLE_SILVER}
@@ -53,10 +52,10 @@ export default class Espy extends React.Component {
           region={this.state.mapRegion}
           onRegionChange={this._handleMapRegionChange}>
           <MapView.Marker coordinate={{ latitude, longitude }}>
-              <View style={styles.radius}>
-                <View style={styles.marker} />
-              </View>
-            </MapView.Marker>
+            <View style={styles.radius}>
+              <View style={styles.marker} />
+            </View>
+          </MapView.Marker>
         </MapView>
       </View>
     )
@@ -88,11 +87,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff'
   },
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
+    flex: 1,
+    zIndex: -1
   }
 });
 
@@ -119,3 +115,23 @@ const styles = StyleSheet.create({
 //   }
 //   this.setState({ location, mapRegion })
 // }
+
+
+// <MapView
+//   showsMyLocationButton={true}
+//   showsPointsOfInterest={true}
+//   showsScale={true}
+//   toolbarEnabled={true}
+//   legalLabelInsets={{ top: 0, bottom: 0, right: 0, left: 0 }}
+//   showsUserLocation={true}
+//   provider={MapView.PROVIDER_GOOGLE}
+//   customMapStyle={MAP_STYLE_SILVER}
+//   style={styles.map}
+//   region={this.state.mapRegion}
+//   onRegionChange={this._handleMapRegionChange}>
+//   <MapView.Marker coordinate={{ latitude, longitude }}>
+//     <View style={styles.radius}>
+//       <View style={styles.marker} />
+//     </View>
+//   </MapView.Marker>
+// </MapView>
---
+++ app/places.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+const homePlace = { description: 'Home', geometry: { location: { lat: 18.9256, lng: 72.8242 } } };
+const workPlace = { description: 'Work', geometry: { location: { lat: 18.9256, lng: 72.8242 } } };
+
+const GooglePlacesInput = () => {
+    return (
+        <GooglePlacesAutocomplete
+            placeholder='Search'
+            minLength={2}
+            autoFocus={false}
+            returnKeyType={'search'}
+            listViewDisplayed='auto'
+            fetchDetails={true}
+            renderDescription={(row) => row.description} // custom description render
+            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                console.log("data ------ ", data);
+                console.log("details -----------", details);
+            }}
+            getDefaultValue={() => {
+                return ''; // text input default value
+            }}
+            query={{
+                key: 'AIzaSyCRBjU3Fqjv4Fm0xys-DRbiFE1tpXbqZ08',
+                language: 'en'
+            }}
+            styles={{
+                zIndex: 100,
+                textInputContainer: {
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0
+                },
+                textInput: {
+                    marginLeft: 0,
+                    marginRight: 0,
+                    height: 38,
+                    color: '#5d5d5d',
+                    fontSize: 16
+                },
+                predefinedPlacesDescription: {
+                    color: '#1faadb'
+                },
+            }}
+
+            currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+            currentLocationLabel="Current location"
+            nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+            GooglePlacesSearchQuery={{
+                // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                rankby: 'distance',
+                types: 'food'
+            }}
+
+            filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+            predefinedPlaces={[homePlace, workPlace]}
+
+            debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+            renderRightButton={() => <Text>Custom text after the inputg</Text>}
+        />
+    );
+}
+
+export default GooglePlacesInput
