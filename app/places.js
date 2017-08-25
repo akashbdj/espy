bd6a7d9361fd30e4736f503251ce8c928c92d1b8
akash-bhardwaj <akashbdj@gmail.com>
2017-08-26 00:54:01 +0530

    style changes

--- app/places.js
+++ app/places.js
@@ -7,60 +7,69 @@ const workPlace = { description: 'Work', geometry: { location: { lat: 18.9256, l
 
 const GooglePlacesInput = () => {
     return (
-        <GooglePlacesAutocomplete
-            placeholder='Search'
-            minLength={2}
-            autoFocus={false}
-            returnKeyType={'search'}
-            listViewDisplayed='auto'
-            fetchDetails={true}
-            renderDescription={(row) => row.description} // custom description render
-            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                console.log("data ------ ", data);
-                console.log("details -----------", details);
-            }}
-            getDefaultValue={() => {
-                return ''; // text input default value
-            }}
-            query={{
-                key: '',
-                language: 'en'
-            }}
-            styles={{
-                zIndex: 100,
-                textInputContainer: {
-                    backgroundColor: 'rgba(0,0,0,0)',
-                    borderTopWidth: 0,
-                    borderBottomWidth: 0
-                },
-                textInput: {
-                    marginLeft: 0,
-                    marginRight: 0,
-                    height: 38,
-                    color: '#5d5d5d',
-                    fontSize: 16
-                },
-                predefinedPlacesDescription: {
-                    color: '#1faadb'
-                },
-            }}
+        <View style={{
+            backgroundColor: 'white',
+            position: 'absolute',
+            zIndex: 2000,
+            top: 20,
+            left: 10,
+            right: 10
+        }}>
+            <GooglePlacesAutocomplete
+                placeholder='Search'
+                minLength={2}
+                autoFocus={false}
+                returnKeyType={'search'}
+                listViewDisplayed='auto'
+                fetchDetails={true}
+                renderDescription={(row) => row.description} // custom description render
+                onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                    console.log("data ------ ", data);
+                    console.log("details -----------", details);
+                }}
+                getDefaultValue={() => {
+                    return ''; // text input default value
+                }}
+                query={{
+                    key: '',
+                    language: 'en'
+                }}
+                styles={{
+                    textInputContainer: {
+                        backgroundColor: 'rgba(0,0,0,0)',
+                        borderTopWidth: 0,
+                        borderBottomWidth: 0
+                    },
+                    textInput: {
+                        marginLeft: 0,
+                        marginRight: 0,
+                        height: 38,
+                        color: '#5d5d5d',
+                        fontSize: 16
+                    },
+                    predefinedPlacesDescription: {
+                        color: '#1faadb'
+                    },
+                }}
+                nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                GooglePlacesSearchQuery={{
+                    // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                    rankby: 'distance',
+                    types: 'food'
+                }}
 
-            currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-            currentLocationLabel="Current location"
-            nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-            GooglePlacesSearchQuery={{
-                // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                rankby: 'distance',
-                types: 'food'
-            }}
+                filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+      
 
-            filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-            predefinedPlaces={[homePlace, workPlace]}
-
-            debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-            renderRightButton={() => <Text>Custom text after the inputg</Text>}
-        />
+                debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+                renderRightButton={() => <Text>Yo</Text>}
+            />
+        </View>
     );
 }
 
 export default GooglePlacesInput
+
+// currentLocation = { true}
+// predefinedPlaces = { [homePlace, workPlace]}
+// currentLocationLabel = "Current location"
