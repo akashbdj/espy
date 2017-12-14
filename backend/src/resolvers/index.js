c6d32927bb2cac35c69308e815726a1e17075364
Akash Bhardwaj <akashbdj@gmail.com>
2017-12-14 23:14:26 +0530

    hashing password using bcrypt

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt'
+
 const MongoClient = require('mongodb').MongoClient
 const URL = 'mongodb://127.0.0.1:27017/espy'
 
@@ -5,6 +7,15 @@ const connectToDB = async (url = URL) => await MongoClient.connect(url)
 
 export default {
     Query: {
+        async getPerson (_, { email }) {
+            const db = await connectToDB()
+            const people = db.collection('people')
+
+            const person = await people.findOne({ email })
+            console.log('PERSON IN DB : ', person)
+            return person
+        },
+
         async getPeopleNearLocation (_, { latitude, longitude, distance = 1000 * 50 }) {
             const db = await connectToDB()
             const loc = {
@@ -19,29 +30,6 @@ export default {
                     loc: {
                         $nearSphere: {
                             $geometry: loc,
-                            $maxDistance: distance
-                        }
-                    }
-                })
-                .toArray()
-
-            return nearestPeople.map((person) => {
-                const { id, loc: { coordinates: [longitude, latitude] } } = person
-                return { id, longitude, latitude }
-            })
-        },
-
-        async getPeopleNearPerson (_, { id, distance = 1000 * 50 }) {
-            const db = await connectToDB()
-
-            const people = db.collection('people')
-            const person = await people.findOne({ id })
-
-            const nearestPeople = await people
-                .find({
-                    loc: {
-                        $nearSphere: {
-                            $geometry: person.loc,
                             $maxDistance: distance
                         }
                     }
@@ -72,6 +60,15 @@ export default {
             )
 
             return { id, longitude, latitude }
+        },
+
+        async registerPerson (_, { name, email, password }) {
+            const db = await connectToDB()
+            const people = db.collection('people')
+            const hashPassword = await bcrypt.hash(password, 12)
+
+            people.insert({ name, email, password: hashPassword }, () => db.close())
+            return { name, email, password: hashPassword }
         }
     }
 }
--- backend/src/schema/index.js
+++ backend/src/schema/index.js
@@ -1,25 +1,39 @@
 import { makeExecutableSchema } from 'graphql-tools'
-import resolvers from './resolvers'
+import resolvers from '../resolvers'
 
 const typeDefs = `
-  type Person {
-    id: ID!
-    longitude: Float!
-    latitude: Float!
-  }
+	type Person {
+		id: ID!
+		name: String!
+        email: String!
+        password: String!
+	    location: Location!
+	}
 
-  type Query {
-    getPeopleNearPerson(id: ID!, distance: Int): [Person]!
-    getPeopleNearLocation(longitude: Float!, latitude: Float!): [Person]! 
-  }
+	type Location {
+		longitude: Float!
+		latitude: Float!
+	}
 
-  type Mutation {
-    addPersonLocation (
-      id: ID!
-      longitude: Float!
-      latitude: Float!
-    ): Person
-  }
+	type Query {
+        getPerson(email: String!): Person
+		getPeopleNearPerson(id: ID!, distance: Int): [Person]!
+		getPeopleNearLocation(longitude: Float!, latitude: Float!): [Person]! 
+	}
+
+	type Mutation {
+        registerPerson(
+            name: String!
+            email: String!
+            password: String!
+        ): Person
+
+		addPersonLocation(
+            id: ID!
+            latitude: Float!
+            longitude: Float!
+		): Person
+	}
 `
 
 const Schema = makeExecutableSchema({ typeDefs, resolvers })
